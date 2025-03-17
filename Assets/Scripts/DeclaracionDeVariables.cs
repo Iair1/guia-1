54d47dfc6f3002aedcf0da46dc2087b5459755b3
Iair1 <iairgreb@gmail.com>
2025-03-17 12:14:14 -0300

    Variables declaradas e inicializadas

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int cantidadAlumnos;
+    float alturaPersonaEnM;
+    string domiclioUsuario;
+    bool aceptaTerminosYCondiciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cantidadAlumnos = 27;
+        alturaPersonaEnM = 1.74f;
+        domiclioUsuario = "Montañeses 2635";
+        aceptaTerminosYCondiciones = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
